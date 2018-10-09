8c5aad7a549f82201e21bc5506bd4fd8117dcc71
Tiffany Carter <tafby88@gmail.com>
2018-10-09 04:10:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,8 +36,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  return array
+  var newArray = array.slice(1)
+  return newArray
 }
 function destructivelyRemoveElementFromEndOfArray(arry) {
   array.slice(0, array.length - 1)
