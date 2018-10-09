03315417b11fd3e0069793967e62b7adcf7e3f1c
Tiffany Carter <tafby88@gmail.com>
2018-10-09 04:04:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  return array
   
 }
 function addElementToEndOfArray(array,element) {
